c93537ce367bd21792eedaffe8a5b9a1d7259509
Acenspades808 <mickelsen.k.wyatt@gmail.com>
2019-12-13 18:42:17 -0700

    Update groupmulitadd.ps1

--- groupmulitadd.ps1
+++ groupmulitadd.ps1
@@ -2,6 +2,6 @@
 $UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 Import-PSSession $Session
-foreach($group in (get-content "C:\Users\wyatt.mickelsen\Downloads\groups.txt")){
-    Add-UnifiedGroupLinks -Identity "$group" -LinkType Members -Links jasenh@safelinkinternet.com
+foreach($group in (get-content "FILEPATH")){
+    Add-UnifiedGroupLinks -Identity "$group" -LinkType Members -Links USER'S_EMAIL
 }
