dc3d5a0685f50c0f5bf94851eff181e335976325
Acenspades808 <mickelsen.k.wyatt@gmail.com>
2019-12-13 18:43:18 -0700

    Delete groupmulitadd.ps1

--- groupmulitadd.ps1
+++
@@ -1,7 +0,0 @@
-<# Add the specified user to multiple O365 groups. #>
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-Import-PSSession $Session
-foreach($group in (get-content "FILEPATH")){
-    Add-UnifiedGroupLinks -Identity "$group" -LinkType Members -Links USER'S_EMAIL
-}
